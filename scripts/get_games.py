104f42299494d5aec931ef5d54f24d8a72bcdf1b
Gregor Voigts <gregor.voigts@gmail.com>
2023-05-15 11:42:47 +0200

    get games for all european leagues

--- scripts/get_games.py
+++ scripts/get_games.py
@@ -6,24 +6,44 @@ import schemas
 from tinydb import TinyDB, Query
 from riotwatcher import LolWatcher, ApiError
 
-db = TinyDB('pro_games.json')
 PlayerQ = Query()
+
+dbs = {'LEC':TinyDB('pro_games_LEC.json'),
+       'EM':TinyDB('pro_games_EM.json'),
+       'PRM':TinyDB('pro_games_PRM.json'),
+       'TCL':TinyDB('pro_games_TCL.json'),
+       'EBL':TinyDB('pro_games_EBL.json'),
+       'ESLOL':TinyDB('pro_games_ESLOL.json'),
+       'GLL':TinyDB('pro_games_GLL.json'),
+       'HM':TinyDB('pro_games_HM.json'),
+       'LFL':TinyDB('pro_games_LFL.json'),
+       'LFL2':TinyDB('pro_games_LFL2.json'),
+       'LPLOL':TinyDB('pro_games_LPLOL.json'),
+       'NLC':TinyDB('pro_games_NLC.json'),
+       'PGN':TinyDB('pro_games_PGN.json'),
+       'SL':TinyDB('pro_games_SL.json'),
+       'UL':TinyDB('pro_games_UL.json')}
 
 # Load players
 players_json = parse_file_as(List[schemas.Player],"players.json")
 
 # Init Riot API
-lol_watcher = LolWatcher('RGAPI-a536f78e-70e0-48ac-ba9f-a47842f6227f')
+lol_watcher = LolWatcher('RGAPI-8de519e2-6856-4812-9e74-1ada5a85b7ee')
 my_region = 'euw1'
 
 for player in players_json:
-    res = db.search(PlayerQ.puuid == player.puuid)
+    if player.puuid == None:
+        continue
+    if player.league == None:
+        print(f'Missing league for {player.name}')
+        continue
+    res = dbs[player.league].search(PlayerQ.puuid == player.puuid)
     playerWGames: schemas.PlayerWGames
     if len(res) != 0:
         playerWGames = schemas.PlayerWGames.parse_obj(res[0])
     else:
-        playerWGames = schemas.PlayerWGames(name=player.name, puuid=player.puuid)
-        db.insert(playerWGames.dict())
+        playerWGames = schemas.PlayerWGames(name=player.name, puuid=player.puuid, summ_name=None, league=player.league)
+        dbs[player.league].insert(playerWGames.dict())
     
     while True:
         # Find next game without information in Stored games
@@ -33,8 +53,8 @@ for player in players_json:
             # Get gameData from Riot
             game_details = lol_watcher.match.by_id(my_region,game.matchId)
             # Update model and DB
-            game_details['info']['matchId'] = game.matchId
-            playerWGames.games[index] = schemas.Game.parse_obj(game_details['info'])
+            game_details['info']['matchId'] = game.matchId # type: ignore
+            playerWGames.games[index] = schemas.Game.parse_obj(game_details['info']) # type: ignore
             '''partcipant = [x for x in game_details['info']['participants'] if x['puuid'] == playerWGames.puuid][0]
             game.championId = partcipant['championId']
             game.championName = partcipant['championName']
@@ -43,47 +63,47 @@ for player in players_json:
             game.goldEarned = partcipant['goldEarned']
             game.totalDamageDealtToChampions = partcipant['totalDamageDealtToChampions']
             game.win = partcipant['win']'''
-            db.update({'games':playerWGames.dict()['games']},PlayerQ.puuid == playerWGames.puuid)
+            dbs[player.league].update({'games':playerWGames.dict()['games']},PlayerQ.puuid == playerWGames.puuid)
 
             # update latestGameTimestamp if necessery
-            if playerWGames.games[index].gameEndTimestamp > playerWGames.lastGameTimestamp:
-                playerWGames.lastGameTimestamp = playerWGames.games[index].gameEndTimestamp
-                db.update({'lastGameTimestamp':playerWGames.games[index].gameEndTimestamp},PlayerQ.puuid == playerWGames.puuid)
+            if playerWGames.games[index].gameEndTimestamp > playerWGames.lastGameTimestamp: # type: ignore
+                playerWGames.lastGameTimestamp = playerWGames.games[index].gameEndTimestamp # type: ignore
+                dbs[player.league].update({'lastGameTimestamp':playerWGames.games[index].gameEndTimestamp},PlayerQ.puuid == playerWGames.puuid)
 
         print(f'All {len(playerWGames.games)} games from {playerWGames.name} where gathered')
         
         # if latest game in list was in the last 12 hours go to the next player
-        if mktime((datetime.now() - timedelta(hours=12)).timetuple()) <= playerWGames.lastGameTimestamp/1000:
+        if mktime((datetime.now() - timedelta(hours=12)).timetuple()) <= playerWGames.lastGameTimestamp/1000: # type: ignore
             print(f'Stop search for Games for {playerWGames.name}. Games up to date')
             break
 
         # Get next gameIds from Riot
-        idList = lol_watcher.match.matchlist_by_puuid(my_region,playerWGames.puuid,start_time=playerWGames.getStartTime(),end_time=playerWGames.getEndTime(), count=100,type='ranked')
+        idList = lol_watcher.match.matchlist_by_puuid(my_region,playerWGames.puuid,start_time=playerWGames.getStartTime(),end_time=playerWGames.getEndTime(), count=100,type='ranked') # type: ignore
 
         # adapt prediction of days needed to play games
-        if len(idList) >= 95:
+        if len(idList) >= 95: # type: ignore
             playerWGames.daysCount -= 5
-            db.update({'daysCount':playerWGames.daysCount},PlayerQ.puuid == playerWGames.puuid)
+            dbs[player.league].update({'daysCount':playerWGames.daysCount},PlayerQ.puuid == playerWGames.puuid)
         # if end time is in future, len isnt correkt
-        elif len(idList) <= 55 and mktime(datetime.now().timetuple()) > playerWGames.getEndTime():
+        elif len(idList) <= 55 and mktime(datetime.now().timetuple()) > playerWGames.getEndTime():# type: ignore
             playerWGames.daysCount += 5
-            db.update({'daysCount':playerWGames.daysCount},PlayerQ.puuid == playerWGames.puuid)
+            dbs[player.league].update({'daysCount':playerWGames.daysCount},PlayerQ.puuid == playerWGames.puuid)
         
         # If result is 100 games, cant be sure to dont miss games. 
         # So ignore result and try again next time with less days
-        if len(idList) == 100:
+        if len(idList) == 100:# type: ignore
             continue
 
         # If no games set current time or end time as new Start time
-        if len(idList) == 0:
-            playerWGames.lastGameTimestamp = min(mktime(datetime.now().timetuple())*1000,playerWGames.getEndTime()*1000)
-            db.update({'lastGameTimestamp':playerWGames.lastGameTimestamp},PlayerQ.puuid == playerWGames.puuid)
+        if len(idList) == 0:# type: ignore
+            playerWGames.lastGameTimestamp = min(mktime(datetime.now().timetuple())*1000,playerWGames.getEndTime()*1000)# type: ignore
+            dbs[player.league].update({'lastGameTimestamp':playerWGames.lastGameTimestamp},PlayerQ.puuid == playerWGames.puuid)
             continue
 
         for id in idList:
-            playerWGames.games.append(schemas.Game(matchId=id))
+            playerWGames.games.append(schemas.Game(matchId=id))# type: ignore
         # Store ids to DB
-        db.update({'games': playerWGames.dict()['games']},PlayerQ.puuid == playerWGames.puuid)
-        print(f'Added {len(idList)} Games to {playerWGames.name}.')
+        dbs[player.league].update({'games': playerWGames.dict()['games']},PlayerQ.puuid == playerWGames.puuid)
+        print(f'Added {len(idList)} Games to {playerWGames.name}.')# type: ignore
 
     
--- scripts/schemas.py
+++ scripts/schemas.py
@@ -5,17 +5,22 @@ import numpy as np
 
 start2023: int = 1672527600000
 
+
 class BaseModel(PydanticBaseModel):
     class Config:
         orm_mode = True
 
+
 class Patch(BaseModel):
-    patch:str
-    date:int
+    patch: str
+    date: int
+
+
 class Player(BaseModel):
     name: str
     puuid: Optional[str]
-    summ_name : Optional[str]
+    summ_name: Optional[str]
+    league: Optional[str]
 
     def __eq__(self, other):
         return self.name == other.name
@@ -23,17 +28,20 @@ class Player(BaseModel):
     def __hash__(self):
         return hash(self.name)
 
+
 class Participant(BaseModel):
     deaths: int
     championId: int
     championName: str
     goldEarned: int
     kills: int
-    teamPosition:str
-    puuid:str
+    teamPosition: str
+    puuid: str
     totalDamageDealtToChampions: int
-    win:bool
-    teamId:int
+    win: bool
+    teamId: int
+
+
 class Game(BaseModel):
     matchId: str
     gameStartTimestamp: Optional[int]
@@ -51,115 +59,109 @@ class PlayerWGames(Player):
 
     def getEndTime(self):
         return int(self.lastGameTimestamp/1000 + (self.daysCount*24*60*60))
-    
+
     def getStartTime(self):
         return int(self.lastGameTimestamp/1000+120)
-    
+
     @validator('lastGameTimestamp')
-    def must_be_highest_timestamp(cls,v,values):
+    def must_be_highest_timestamp(cls, v, values):
         latest = v
         if not 'games' in values:
             return v
         for game in values['games']:
             if game.gameEndTimestamp != None and game.gameEndTimestamp > latest:
                 latest = game.gameEndTimestamp
-        print(f'{v}:{latest}')
         if v != latest:
             print(f'Unmatching timestamps for {values["name"]}')
         return latest
 
-class LSTM_Player_Data(BaseModel):
-    player_name:str = ""
-    champ_name:str = ""
-    champ:int = 0
-    champ_winrate:float = 0
-    pro_winrate:float = 0
-    gold_avg:float = 0
-    kills_avg:float = 0
-    damage_avg:float = 0
-    deaths_avg:float = 0
+
+class Array_Player_Data(BaseModel):
+    player_name: str = ""
+    champ_name: str = ""
+    champ: int = 0
+    champ_winrate: Optional[float]
+    pro_winrate: Optional[float]
+    gold_avg: Optional[float]
+    kills_avg: Optional[float]
+    damage_avg: Optional[float]
+    deaths_avg: Optional[float]
 
     # normalizes Stats so the better player gets 1 and the oponend
     # gets his procentage of the better players stat
     def normalize_stats(self, oponend):
-        max_gold = max(self.gold_avg,oponend.gold_avg)
-        if max_gold > 0:
-            self.gold_avg = self.gold_avg/max_gold
+        if self.gold_avg != None and oponend.gold_avg != None:
+            max_gold = max(self.gold_avg, oponend.gold_avg)
+            if max_gold > 0:
+                self.gold_avg = self.gold_avg/max_gold
 
-        max_kills = max(self.kills_avg,oponend.kills_avg)
-        if max_kills > 0:
-            self.kills_avg = self.kills_avg/max_kills
+        if self.kills_avg != None and oponend.kills_avg != None:
+            max_kills = max(self.kills_avg, oponend.kills_avg)
+            if max_kills > 0:
+                self.kills_avg = self.kills_avg/max_kills
 
-        max_damage = max(self.damage_avg,oponend.damage_avg)
-        if max_damage > 0:
-            self.damage_avg = self.damage_avg/max_damage
+        if self.damage_avg != None and oponend.damage_avg != None:
+            max_damage = max(self.damage_avg, oponend.damage_avg)
+            if max_damage > 0:
+                self.damage_avg = self.damage_avg/max_damage
 
-        max_deaths = max(self.deaths_avg,oponend.deaths_avg)
-        if max_deaths > 0:
-            self.deaths_avg = self.deaths_avg/max_deaths
+        if self.deaths_avg != None and oponend.deaths_avg != None:
+            max_deaths = max(self.deaths_avg, oponend.deaths_avg)
+            if max_deaths > 0:
+                self.deaths_avg = self.deaths_avg/max_deaths
 
         return self
 
     # Build an numpy array with one Hot for the Champion and all the stats
-    def get_array(self, champ_count):
-        arr = np.zeros(champ_count+6, dtype=float)
-        arr[self.champ] = 1
-        arr[champ_count] = self.champ_winrate
-        arr[champ_count+1] = self.pro_winrate
-        arr[champ_count+2] = self.gold_avg
-        arr[champ_count+3] = self.kills_avg
-        arr[champ_count+4] = self.damage_avg
-        arr[champ_count+5] = self.deaths_avg
+    def get_array(self):
+        arr = np.zeros(6, dtype=float)
+        arr[0] = self.champ_winrate
+        arr[1] = self.pro_winrate
+        arr[2] = self.gold_avg
+        arr[3] = self.kills_avg
+        arr[4] = self.damage_avg
+        arr[5] = self.deaths_avg
         return arr
-    
-class LSTM_Match_Up(BaseModel):
-    red:LSTM_Player_Data
-    blue:LSTM_Player_Data
 
-class LSTM_5Seq(BaseModel):
-    gameId:str
 
-    top:LSTM_Match_Up
-    jng:LSTM_Match_Up
-    mid:LSTM_Match_Up
-    bot:LSTM_Match_Up
-    sup:LSTM_Match_Up
+class Array_Complet(BaseModel):
+    gameId: str = ""
 
-    red_win:int
-    blue_win:int
+    top_blue: Array_Player_Data = Array_Player_Data() # type: ignore
+    jng_blue: Array_Player_Data = Array_Player_Data()# type: ignore
+    mid_blue: Array_Player_Data = Array_Player_Data()# type: ignore
+    bot_blue: Array_Player_Data = Array_Player_Data()# type: ignore
+    sup_blue: Array_Player_Data = Array_Player_Data()# type: ignore
 
-class LSTM_10Seq(BaseModel):
-    gameId:str = ""
+    top_red: Array_Player_Data = Array_Player_Data()# type: ignore
+    jng_red: Array_Player_Data = Array_Player_Data()# type: ignore
+    mid_red: Array_Player_Data = Array_Player_Data()# type: ignore
+    bot_red: Array_Player_Data = Array_Player_Data()# type: ignore
+    sup_red: Array_Player_Data = Array_Player_Data()# type: ignore
 
-    top_blue:LSTM_Player_Data = LSTM_Player_Data()
-    jng_blue:LSTM_Player_Data= LSTM_Player_Data()
-    mid_blue:LSTM_Player_Data= LSTM_Player_Data()
-    bot_blue:LSTM_Player_Data= LSTM_Player_Data()
-    sup_blue:LSTM_Player_Data= LSTM_Player_Data()
+    red_win: int = 0
+    blue_win: int = 0
 
-    top_red:LSTM_Player_Data= LSTM_Player_Data()
-    jng_red:LSTM_Player_Data= LSTM_Player_Data()
-    mid_red:LSTM_Player_Data= LSTM_Player_Data()
-    bot_red:LSTM_Player_Data= LSTM_Player_Data()
-    sup_red:LSTM_Player_Data= LSTM_Player_Data()
-
-    red_win:int = 0
-    blue_win:int = 0
-    
     def get_arrays(self, champ_count):
-        x = np.zeros((10,champ_count+6), dtype=float)
-        x[0] = self.top_blue.normalize_stats(self.top_red).get_array(champ_count)
-        x[1] = self.jng_blue.normalize_stats(self.jng_red).get_array(champ_count)
-        x[2] = self.mid_blue.normalize_stats(self.mid_red).get_array(champ_count)
-        x[3] = self.bot_blue.normalize_stats(self.bot_red).get_array(champ_count)
-        x[4] = self.sup_blue.normalize_stats(self.sup_red).get_array(champ_count)
-        
-        x[5] = self.top_red.normalize_stats(self.top_blue).get_array(champ_count)
-        x[6] = self.jng_red.normalize_stats(self.jng_blue).get_array(champ_count)
-        x[7] = self.mid_red.normalize_stats(self.mid_blue).get_array(champ_count)
-        x[8] = self.bot_red.normalize_stats(self.bot_blue).get_array(champ_count)
-        x[9] = self.sup_red.normalize_stats(self.sup_blue).get_array(champ_count)
+        x = np.zeros(champ_count, dtype=float)
+        # fill champs
+        for player in [self.top_blue, self.jng_blue, self.mid_blue, self.bot_blue, self.sup_blue]:
+            x[player.champ] = 1
+        for player in [self.top_red, self.jng_red, self.mid_red, self.bot_red, self.sup_red]:
+            x[player.champ] = -1
+        # add player info
+        x = np.append(x, (
+            self.top_blue.normalize_stats(self.top_red).get_array(), 
+            self.jng_blue.normalize_stats(self.jng_red).get_array(), 
+            self.mid_blue.normalize_stats(self.mid_red).get_array(), 
+            self.bot_blue.normalize_stats(self.bot_red).get_array(), 
+            self.sup_blue.normalize_stats(self.sup_red).get_array(), 
+            self.top_red.normalize_stats(self.top_blue).get_array(), 
+            self.jng_red.normalize_stats(self.jng_blue).get_array(), 
+            self.mid_red.normalize_stats(self.mid_blue).get_array(), 
+            self.bot_red.normalize_stats(self.bot_blue).get_array(), 
+            self.sup_red.normalize_stats(self.sup_blue).get_array()))
 
-        y = np.array([self.blue_win,self.red_win])
+        y = np.array([self.blue_win, self.red_win])
 
-        return x,y
+        return x, y
